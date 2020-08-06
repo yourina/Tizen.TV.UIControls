9ba0b0159a49d7144799da9f713f70aa6dee5ee7
Rina You <rina6350.you@samsung.com>
2020-08-06 09:30:50 -0400

    Add the TMDb.G

---
+++ sample/TMDb.G/TMDb/TMDbAPIKey.cs
@@ -0,0 +1,8 @@
+namespace TMDb
+{
+    public class TMDbAPIKey
+    {
+        // https://www.themoviedb.org/documentation/api
+        public static readonly string Key = "870755bb1ee864493829202f5afa20bd";
+    }
+}
